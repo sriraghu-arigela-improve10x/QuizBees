2716fc880b6297cd1e285feb7632ce346350964c
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-06-07 11:20:24 +0530

    create design for QuestionsItem xml

--- app/src/main/java/com/example/quizbee/QuestionsActivity.java
+++ app/src/main/java/com/example/quizbee/QuestionsActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.quizbee.databinding.ActivityQuestionsBinding;
+import com.example.quizbee.model.QuizBee;
+
+import java.util.ArrayList;
 
 public class QuestionsActivity extends AppCompatActivity {
     private ActivityQuestionsBinding binding;
+    private ArrayList<QuizBee> quizBees = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
